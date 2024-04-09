9b7d921f3c502b0a5223b241cba61c064e1cacbf
kareemqaraeen <81602126+kareemqaraienn@users.noreply.github.com>
2024-04-08 23:16:19 -0400

    Update fact_table.sql
    add new demographic info

--- sql/fact_table.sql
+++ sql/fact_table.sql
@@ -2,17 +2,12 @@ CREATE TABLE fact_table (
     date_key INTEGER REFERENCES dim_date (date_key),
     education_key INTEGER REFERENCES dim_education (education_key),
     industry_key INTEGER REFERENCES dim_industry (industry_key),
-    demographic_key INTEGER REFERENCES dim_demographic (demographic_key),
-    employment_key INTEGER REFERENCES dim_employment_status (employment_key),
-    Number_of_Employees NUMERIC,  
-    GDP_Value NUMERIC,           
-    Number_of_Graduates NUMERIC, 
-    PRIMARY KEY (date_key, education_key, industry_key, demographic_key, employment_key)
+    employee_demographic_key INTEGER REFERENCES dim_employee_demographic (employee_demographic_key),
+    employment_status_key INTEGER REFERENCES dim_employment_status (employment_key),
+    graduate_demographic_key INTEGER REFERENCES dim_graduate_demographic (graduate_demographic_key),
+
+    number_of_employees NUMERIC,
+    gdp_value NUMERIC,
+    number_of_graduates NUMERIC,
+    PRIMARY KEY (date_key, education_key, industry_key, employee_demographic_key, employment_status_key, graduate_demographic_key)
 );
-
-
-
-
-
-
-
